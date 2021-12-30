35a030c16293c391ba97c0941d8d8da39b77846b
York Yao <me@yorkyao.com>
2021-12-31 07:41:17 +0800

    feat: add sqlite db data

--- src/data.ts
+++ src/data.ts
@@ -49,3 +49,53 @@ export const posts = [
     blogId: 2
   }
 ]
+
+import * as sqlite from 'sqlite3'
+
+const db = new sqlite.Database(':memory:')
+
+function run(sql: string, ...args: unknown[]) {
+  return new Promise<void>((resolve, reject) => {
+    db.run(sql, args, (err) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve()
+      }
+    })
+  })
+}
+
+function all<T>(sql: string, ...args: unknown[]) {
+  return new Promise<T[]>((resolve, reject) => {
+    db.all(sql, args, (err, rows) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(rows)
+      }
+    })
+  })
+}
+
+(async () => {
+  await run('CREATE TABLE IF NOT EXISTS blogs(id, content, meta)')
+  await run('CREATE TABLE IF NOT EXISTS posts(id, content, blogId)')
+
+  await run('INSERT INTO blogs (id, content, meta) VALUES (?, ?, ?)', 1, 'blog 1 content', JSON.stringify({
+    foo: 'bar'
+  }))
+  await run('INSERT INTO blogs (id, content, meta) VALUES (?, ?, ?)', 2, 'blog 2 content', JSON.stringify({
+    bar: 123
+  }))
+
+  await run('INSERT INTO posts (id, content, blogId) VALUES (?, ?, ?)', 11, 'post 11 content', 1)
+  await run('INSERT INTO posts (id, content, blogId) VALUES (?, ?, ?)', 12, 'post 12 content', 1)
+  await run('INSERT INTO posts (id, content, blogId) VALUES (?, ?, ?)', 13, 'post 13 content', 1)
+  await run('INSERT INTO posts (id, content, blogId) VALUES (?, ?, ?)', 21, 'post 21 content', 2)
+  await run('INSERT INTO posts (id, content, blogId) VALUES (?, ?, ?)', 22, 'post 22 content', 2)
+  await run('INSERT INTO posts (id, content, blogId) VALUES (?, ?, ?)', 23, 'post 23 content', 2)
+
+  console.info(await all('SELECT * FROM blogs'))
+  console.info(await all('SELECT * FROM posts'))
+})()
